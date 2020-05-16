acff680d04251f86d1515cf46096365e1ec29bc5
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2020-05-17 07:52:33 +0900

    1-6: implement lottery_n3logn

--- 1-6/lottery_n3logn.cpp
+++ 1-6/lottery_n3logn.cpp
@@ -1,15 +1,17 @@
+#include <algorithm> // for sort()
 #include <iostream>
+#include <progchallenge/search.hpp>
 #include <vector>
 
-bool exist_exactsum(const std::vector<int> &nums, const int sum);
+bool exist_exactsum(std::vector<int> &nums, const int sum);
 
 using namespace std;
 
 int main() {
   // inputs
   vector<int> k{1, 3, 5};
-  //  int m{10};
-  int m{9};
+  int m{10};
+  //  int m{9};
 
   // result
   if (exist_exactsum(k, m))
@@ -20,18 +22,19 @@ int main() {
   return 0;
 }
 
-bool exist_exactsum(const std::vector<int> &nums, const int sum) {
+bool exist_exactsum(std::vector<int> &nums, const int sum) {
+  std::sort(nums.begin(), nums.end()); // for binary search
   const int n = nums.size();
 
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
       for (int k = 0; k < n; ++k) {
-        for (int l = 0; l < n; ++l) {
-          if (nums[i] + nums[j] + nums[k] + nums[l] == sum)
-            return true;
-        }
+        if (progchallenge::binary_search(sum - nums[i] - nums[j] - nums[k],
+                                         nums))
+          return true;
       }
     }
   }
+
   return false;
 }
--- progchallenge/search.cpp
+++ progchallenge/search.cpp
@@ -2,19 +2,22 @@
 
 namespace progchallenge {
 
-bool binary_search(const int x, const std::vector<int> &v){
-    int l=0;
-    int r=v.size();
+bool binary_search(const int x, const std::vector<int> &v) {
+  int l = 0;
+  int r = v.size();
 
-    // search until the range become empty.
-    while(r-l>0){
-        int c = (l+r)/2;
-        if(v.at<int>(c) == x) return true;
-        else if(v[c] < x) l = c+1;
-        else r = c;
-    }
+  // search until the range become empty.
+  while (r - l > 0) {
+    int c = (l + r) / 2;
+    if (v.at(c) == x)
+      return true;
+    else if (v[c] < x)
+      l = c + 1;
+    else
+      r = c;
+  }
 
-    return false;
+  return false;
 }
 
 } // namespace progchallenge
